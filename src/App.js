2132d80c2078a4aa18755197440ced647eaf4d04
santhosh-ramesh <60465183+Santhosh-Ramesh@users.noreply.github.com>
2022-05-15 21:32:51 +0530

    created checkout page along with checkout items and checkout cart functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Navigation from './components/routes/navigation/navigation.component.jsx';
 import Authentication from './components/routes/authentication/authentication.component.jsx';
 import Shop from './components/routes/shop/shop.components.jsx';
+import Checkout from './components/routes/checkout/checkout.components.jsx';
 const App = () => {
   return (
     <Routes>
@@ -10,6 +11,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
         <Route path='auth' element={<Authentication />} />
+        <Route path='checkout' element={<Checkout />} />
       </Route>
     </Routes>
   );
