1fd90670bceb86b3948a6728ed2ca34f6fcb2d34
santhosh-ramesh <60465183+Santhosh-Ramesh@users.noreply.github.com>
2022-06-12 19:20:11 +0530

    Added persistor from redux-presist for local storage use of cart items

--- src/store/store.js
+++ src/store/store.js
@@ -8,16 +8,40 @@
 
 // export const store = createStore(rootReducer, {}, composedEnhancers);
 
-
 import { compose, createStore, applyMiddleware } from 'redux';
+
+import { persistStore, persistReducer } from 'redux-persist';
+
+import storage from 'redux-persist/lib/storage';
+
 import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
   Boolean
 );
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const composedEnhancer =
+  (process.env.NODE_ENV !== 'production' &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+const composedEnhancers = composedEnhancer(applyMiddleware(...middleWares));
+
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
